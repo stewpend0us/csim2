222128b292e60ac7e21498c9421406b654725ca5
lenovo\stewart <stewart.wadsworth@gmail.com>
2017-05-17 20:41:49 -0400

    pretty good spot. need a solver. then an example

--- StrictlyProperBlock.c
+++ StrictlyProperBlock.c
@@ -2,13 +2,13 @@
 #include <string.h>
 #include "StrictlyProperBlock.h"
 
-static void integrator_f(double * const dState, double t, double const * const state, double const * const input, size_t const numStates, size_t const numInputs, void * const storage)
+static void integrator_f(size_t const numStates, double * const dState, double t, double const * const state, size_t const numInputs, double const * const input, void * const storage)
 {
 	assert(numStates == numInputs);
 	memcpy(dState, input, numStates * sizeof(double));
 }
 
-static void integrator_h(double * const output, double t, double const * const state, size_t numStates, size_t const numOutputs, void * const storage)
+static void integrator_h(size_t const numOutputs, double * const output, double t, size_t numStates, double const * const state, void * const storage)
 {
 	assert(numStates == numOutputs);
 	memcpy(output, state, numStates * sizeof(double));
@@ -22,7 +22,7 @@ struct StrictlyProperBlock integrator(size_t numSignals)
 	};
 }
 
-struct SystemStorage system_storage_new(size_t numBlocks, struct StrictlyProperBlock const * const blocks, void(*updateInputs)(double * const * const inputs, double const * const * const outputs))
+struct SystemStorage system_storage_new(size_t numBlocks, struct StrictlyProperBlock const * const blocks, UpdateBlockInputsFunction updateInputs, CalcSystemOutputFunction calcOutput)
 {
 	size_t totalBlockInputs = 0;
 	size_t totalBlockOutputs = 0;
@@ -51,7 +51,7 @@ struct SystemStorage system_storage_new(size_t numBlocks, struct StrictlyProperB
 
 	return (struct SystemStorage)
 	{
-		.numBlocks = numBlocks, .blocks = blocks, .blockInputs = input_storage, .blockOutputs = output_storage, .updateInputs = updateInputs,
+		.numBlocks = numBlocks, .blocks = blocks, .blockInputs = input_storage, .blockOutputs = output_storage, .updateInputs = updateInputs, .calcOutput = calcOutput,
 	};
 }
 
@@ -62,14 +62,33 @@ void system_storage_free(struct SystemStorage const * const storage)
 	free(storage->blockOutputs);
 }
 
-static void system_f(double * const dState, double t, double const * const state, double const * const input, size_t const numStates, size_t const numInputs, void * const storage)
+static void blockSystem_f(size_t const numStates, double * const dState, double t, double const * const state, size_t const numInputs, double const * const input, struct SystemStorage * const storage)
 {
-
+	size_t numBlocks = storage->numBlocks;
+	struct StrictlyProperBlock const * const blocks = storage->blocks;
+	double * const * const blockInputs = storage->blockInputs;
+	double const * const * const blockOutputs = storage->blockOutputs;
+	size_t xi = 0;
+	storage->updateInputs(numBlocks, blocks, blockInputs, blockOutputs, numInputs, input);
+	for (size_t i = 0; i < numBlocks; i++)
+	{
+		blocks[i].f(blocks[i].numStates, &dState[xi], t, &state[xi], blocks[i].numInputs, blockInputs[i], blocks[i].storage);
+		xi += blocks[i].numStates;
+	}
 }
 
-static void system_h(double * const output, double t, double const * const state, size_t numStates, size_t const numOutputs, void * const storage)
+static void blockSystem_h(size_t const numOutputs, double * const output, double t, size_t numStates, double const * const state, struct SystemStorage * const storage)
 {
-
+	size_t numBlocks = storage->numBlocks;
+	struct StrictlyProperBlock const * const blocks = storage->blocks;
+	double * const * const blockOutputs = storage->blockOutputs;
+	size_t xi = 0;
+	for (size_t i = 0; i < numBlocks; i++)
+	{
+		blocks[i].h(blocks[i].numOutputs, blockOutputs[i], t, blocks[i].numStates, &state[xi], blocks[i].storage);
+		xi += blocks[i].numStates;
+	}
+	storage->calcOutput(numOutputs, output, blockOutputs);
 }
 
 struct StrictlyProperBlock blockSystem(size_t numInputs, size_t numOutputs, struct SystemStorage * const storage)
@@ -80,9 +99,8 @@ struct StrictlyProperBlock blockSystem(size_t numInputs, size_t numOutputs, stru
 		totalBlockStates += storage->blocks[i].numStates;
 	}
 
-
 	return (struct StrictlyProperBlock)
 	{
-		.numStates = totalBlockStates, .numInputs = numInputs, .numOutputs = numOutputs, .f = system_f, .h = system_h, .storage = storage,
+		.numStates = totalBlockStates, .numInputs = numInputs, .numOutputs = numOutputs, .f = blockSystem_f, .h = blockSystem_h, .storage = storage,
 	};
 }
--- StrictlyProperBlock.h
+++ StrictlyProperBlock.h
@@ -7,11 +7,14 @@ struct StrictlyProperBlock
 	size_t const numInputs;
 	size_t const numOutputs;
 	void * storage;
-	void(*f)(double * const dState, double t, double const * const state, double const * const input, size_t const numStates, size_t const numInputs,  void * const storage);
-	void(*h)(double * const output, double t, double const * const state, size_t numStates, size_t const numOutputs, void * const storage);
+	void(*f)(size_t const numStates, double * const dState, double t, double const * const state, size_t const numInputs, double const * const input, void * const storage);
+	void(*h)(size_t const numOutputs, double * const output, double t, size_t numStates, double const * const state, void * const storage);
 };
 
 struct StrictlyProperBlock integrator(size_t numSignals);
+
+typedef void(*UpdateBlockInputsFunction)(size_t numBlocks, struct StrictlyProperBlock const * const blocks, double * const * const inputs, double const * const * const outputs, size_t numSystemInputs, double const * const systemInput);
+typedef void(*CalcSystemOutputFunction)(size_t numOutputs, double * const outputs, double const * const * const blockOutputs);
 
 struct SystemStorage
 {
@@ -19,10 +22,11 @@ struct SystemStorage
 	struct StrictlyProperBlock const * const blocks;
 	double * const * const blockInputs;
 	double * const * const blockOutputs;
-	void(*updateInputs)(double * const * const inputs, double const * const * const outputs);
+	UpdateBlockInputsFunction updateInputs;
+	CalcSystemOutputFunction calcOutput;
 };
 
-struct SystemStorage system_storage_new(size_t numBlocks, struct StrictlyProperBlock const * const blocks, void(*updateInputs)(double * const * const inputs, double const * const * const outputs));
+struct SystemStorage system_storage_new(size_t numBlocks, struct StrictlyProperBlock const * const blocks, UpdateBlockInputsFunction updateInputs, CalcSystemOutputFunction calcOutputs);
 void system_storage_free(struct SystemStorage const * const storage);
 
 struct StrictlyProperBlock blockSystem(size_t numInputs, size_t numOutputs, struct SystemStorage * const storage);
