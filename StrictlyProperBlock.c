cd1d9499d6b9b39d5cce49c286586ae7cc13e924
lenovo\stewart <stewart.wadsworth@gmail.com>
2017-05-24 23:00:35 -0400

    good start on rk4

--- StrictlyProperBlock.c
+++ StrictlyProperBlock.c
@@ -8,34 +8,49 @@ void euler
 
 }
 
-void rk4
+static void rk4_physics_step
 (
-	double * const Y,
-	double * const Xi,
-	struct StrictlyProperBlock const * const block,
-	size_t const num_t,
-	double const * const t,
-	double const * const U
+	size_t const xi,
+	double * const dX, // 5 * num_X buffer for rk4 algorithm
+	double * const Xi, // 1 * num_X current state
+	double const ti, // current time
+	double const dt, // time step
+	double const dt2, // time step / 2
+	size_t const ui,
+	double const * const U, // 3 * num_U input at time t, t + dt/2, t + dt
+	PhysicsFunction const f,
+	void * const storage
 )
 {
+	size_t i;
+	double * const A = &dX[0 * xi];
+	double * const B = &dX[1 * xi];
+	double * const C = &dX[2 * xi];
+	double * const D = &dX[3 * xi];
+	double * const Xt = &dX[4 * xi];
+	double const * const Uti = &U[0 * ui];
+	double const * const Ut2 = &U[1 * ui];
+	double const * const Ut1 = &U[2 * ui];
+	double const t2 = ti + dt2;
+	double const t1 = ti + dt;
 
-	size_t yi = block->numOutputs;
-	size_t xi = block->numStates;
-	void * storage = block->storage;
+	f(xi, A, ti, xi, Xi, ui, Uti, storage);
+	for (i = 0; i < xi; i++)
+		Xt[i] = Xi[i] + A[i] * dt2;
+	f(xi, B, t2, xi, Xt, ui, Ut2, storage);
+	for (i = 0; i < xi; i++)
+		Xt[i] = Xi[i] + B[i] * dt2;
+	f(xi, C, t2, xi, Xt, ui, Ut2, storage);
+	for (i = 0; i < xi; i++)
+		Xt[i] = Xi[i] + C[i] * dt;
+	f(xi, D, t1, xi, Xt, ui, Ut1, storage);
+	for (i = 0; i < xi; i++)
+		Xi[i] += dt * (A[i] + 2 * B[i] + 2 * C[i] + D[i]) / 6;
 
-	for (size_t i = 0; i < num_t; i++)
-	{
-		block->h(yi, &Y[i*yi], t[i], xi, Xi, storage);
-
-	}
 	//for i = 1:numpts - 1
 	//	[A, output(i, :)] = f.physics(time(i), state(i, :), input(i, :));
 
-	//if endEarly(i, time, state, output)
-	//	beenEnded = true;
-	//break;
-	//end
-	//	B = f.physics(t2(i), state(i, :) + A*dt2, input2(i, :));
+	//B = f.physics(t2(i), state(i, :) + A*dt2, input2(i, :));
 	//C = f.physics(t2(i), state(i, :) + B*dt2, input2(i, :));
 	//D = f.physics(time(i + 1), state(i, :) + C*dt, input(i + 1, :));
 
--- StrictlyProperBlock.h
+++ StrictlyProperBlock.h
@@ -25,6 +25,12 @@ typedef void(*OutputFunction)(
 	void * const storage
 	);
 
+typedef void(*InputFunction)(
+	size_t const numInputs,
+	double * const U,
+	double const t
+	);
+
 // data types:
 
 // StrictlyProperBlock data
@@ -45,10 +51,14 @@ void euler
 // rk4 solver
 void rk4
 (
-	double * const Y,
-	double * const Xi,
-	struct StrictlyProperBlock const * const block,
-	size_t const num_t,
-	double const * const t,
-	double const * const U
+	size_t const xi,
+	double * const dX, // 5 * num_X buffer for rk4 algorithm
+	double * const Xi, // 1 * num_X current state
+	double const t, // current time
+	double const dt, // time step
+	double const dt2, // time step / 2
+	size_t const ui,
+	double const * const U, // 3 * num_U input at time t, t + dt2, t + dt
+	PhysicsFunction const f,
+	void * const storage
 );
