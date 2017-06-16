36b1ba739e676564cfde2f4dfd676b4f600f0d3f
lenovo\stewart <stewart.wadsworth@gmail.com>
2017-06-16 17:24:25 -0400

    second order system should vectorize now

--- csim2/secondOrderSystem.c
+++ csim2/secondOrderSystem.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "secondOrderSystem.h"
 
 static void physics
@@ -11,22 +12,32 @@ static void physics
 	struct secondOrderSystemStorage const * const storage
 )
 {
-	double zeta;
-	double wn;
-	double wn2;
-	size_t i1;
-	size_t i2;
-	for (size_t i = 0; i < numInputs; i++)
-	{
-		i1 = i * 2;
-		i2 = i1 + 1;
-		zeta = storage[i].zeta;
-		wn = storage[i].omega_n;
-		wn2 = wn*wn;
+	double * const state1 = state;
+	double * const state2 = state + numInputs;
+	double * const dState1 = dState;
+	double * const dState2 = dState + numInputs;
 
-		dState[i1] = state[i2];
-		dState[i2] = -2*zeta*wn*state[i2] - wn2*state[i1] + wn2*input[i];
-	}
+	memcpy(dState1, state2, numInputs * sizeof(double));
+
+	for (size_t i = 0; i < numInputs; i++)
+		dState2[i] = -2 * storage[i].zeta * storage[i].omega_n * state2[i] - storage[i].omega_n * storage[i].omega_n * ( state1[i] - input[i] );
+	
+	//double zeta;
+	//double wn;
+	//double wn2;
+	//size_t i1;
+	//size_t i2;
+	//for (size_t i = 0; i < numInputs; i++)
+	//{
+	//	i1 = i * 2;
+	//	i2 = i1 + 1;
+	//	zeta = storage[i].zeta;
+	//	wn = storage[i].omega_n;
+	//	wn2 = wn*wn;
+
+	//	dState[i1] = state[i2];
+	//	dState[i2] = -2*zeta*wn*state[i2] - wn2*state[i1] + wn2*input[i];
+	//}
 }
 
 static void output
@@ -39,12 +50,15 @@ static void output
 	struct secondOrderSystemStorage const * const storage
 )
 {
-	size_t i1;
-	for (size_t i = 0; i < numOutputs; i++)
-	{
-		i1 = i * 2;
-		output[i] = state[i1];
-	}
+	double * const state1 = state;
+	memcpy(output, state1, numOutputs * sizeof(double));
+
+	//size_t i1;
+	//for (size_t i = 0; i < numOutputs; i++)
+	//{
+	//	i1 = i * 2;
+	//	output[i] = state[i1];
+	//}
 }
 
 struct StrictlyProperBlock secondOrderSystem(size_t const numBlocks, struct secondOrderSystemStorage * const storage)
@@ -52,7 +66,7 @@ struct StrictlyProperBlock secondOrderSystem(size_t const numBlocks, struct seco
 	struct StrictlyProperBlock b;
 	b.numInputs = numBlocks;
 	b.numOutputs = numBlocks;
-	b.numStates = numBlocks*2;
+	b.numStates = 2 * numBlocks;
 	b.storage = storage;
 	b.f = physics;
 	b.h = output;
