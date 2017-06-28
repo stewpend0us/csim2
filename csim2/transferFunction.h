174cf03155681123e14ac8461dc9ee2b3fe2770c
lenovo\stewart <stewart.wadsworth@gmail.com>
2017-06-27 23:54:35 -0400

    first stab at general transfer function

---
+++ csim2/transferFunction.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "StrictlyProperBlock.h"
+
+struct transferFunctionStorage
+{
+	size_t numSize;
+	size_t denSize;
+	double const * numerator;
+	double const * denominator;
+};
+
+struct StrictlyProperBlock transferFunction(size_t const numBlocks, struct transferFunctionStorage * const storage);
