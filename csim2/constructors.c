6f894573d58c8c84c74e6835c8d986e573bc76d5
lenovo\stewart <stewart.wadsworth@gmail.com>
2017-07-10 23:03:54 -0400

    more cleaning
    removed even more duplicate code by making a generic block_new block_free function. Just copies a block from the stack to the heap (which is all we were doing before but with a pair of functions per type)

--- csim2/constructors.c
+++ csim2/constructors.c
@@ -1,7 +1,7 @@
 #include <string.h>
 #include "constructors.h"
 
-static struct StrictlyProperBlock * _new(struct StrictlyProperBlock const stackb)
+struct StrictlyProperBlock * block_new(struct StrictlyProperBlock const stackb)
 {
 	struct StrictlyProperBlock * heapb = malloc(sizeof(struct StrictlyProperBlock));
 	if (heapb)
@@ -9,57 +9,7 @@ static struct StrictlyProperBlock * _new(struct StrictlyProperBlock const stackb
 	return heapb;
 }
 
-struct StrictlyProperBlock * integrator_new(size_t const numBlocks)
+void block_free(struct StrictlyProperBlock * heapb)
 {
-	struct StrictlyProperBlock stackb = integrator(numBlocks);
-	return _new(stackb);
-}
-void integrator_free(struct StrictlyProperBlock * b)
-{
-	free(b);
-}
-
-struct StrictlyProperBlock * firstOrderLag_new(size_t const numBlocks, double * const tau)
-{
-	struct StrictlyProperBlock stackb = firstOrderLag(numBlocks, tau);
-	return _new(stackb);
-}
-void firstOrderLag_free(struct StrictlyProperBlock * b)
-{
-	free(b);
-}
-
-struct StrictlyProperBlock * secondOrderSystem_new(size_t const numBlocks, struct secondOrderSystemStorage * const storage)
-{
-	struct StrictlyProperBlock stackb = secondOrderSystem(numBlocks, storage);
-	return _new(stackb);
-}
-void secondOrderSystem_free(struct StrictlyProperBlock * b)
-{
-	free(b);
-}
-
-struct StrictlyProperBlock * transferFunction_new(size_t const numBlocks, struct transferFunctionStorage * const storage)
-{
-	struct StrictlyProperBlock stackb = transferFunction(numBlocks, storage);
-	return _new(stackb);
-}
-void transferFunction_free(struct StrictlyProperBlock * b)
-{
-	free(b);
-}
-
-struct StrictlyProperBlock * blockSystem_new
-(
-	size_t const numSystemInputs,
-	size_t const numSystemOutputs,
-	struct BlockSystemStorage * const storage
-)
-{
-	struct StrictlyProperBlock stackb = blockSystem(numSystemInputs, numSystemOutputs, storage);
-	return _new(stackb);
-}
-void blockSystem_free(struct StrictlyProperBlock * b)
-{
-	free(b);
+	free(heapb);
 }
