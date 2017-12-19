b09cb85ebe22cb1417e75109083ca7d9ad0930bc
stewart-lenovo <stewart.wadsworth@gmail.com>
2017-12-18 22:40:00 -0500

    example is working well

--- examples/mass_spring_damper_example.c
+++ examples/mass_spring_damper_example.c
@@ -1,7 +1,8 @@
 #include "multistep.h"
 #include "dbg.h"
 #include <math.h>
-#include<time.h>
+#include <time.h>
+#include <string.h>
 
 struct mass_storage
 {
@@ -73,10 +74,10 @@ static void util(
 	double const velocity = state[0];
 	double const position = state[1];
 	int const space = 40;
-	int const pos = floor(space*position)+space;
+	int pos = round(position*space)+space;
 	double const thresh = .1;
 	printf(":");
-	for (int i = 0; i < pos; i++)
+	for (int i = 0; i < pos+1; i++)
 		printf(" ");
 	if (velocity < -thresh)
 		printf("/");
@@ -84,28 +85,48 @@ static void util(
 		printf("\\");
 	else
 		printf("|");
-	for (int i = pos; i<2*space; i++)
+	for (int i = pos; i<2*space+1; i++)
 		printf(" ");
 	printf(":\n");
-	//return;
-	//printf("%f\n",velocity);
 	delay(storage->dt*1000);
 }
 
-
-
-int main()
+int main(int argc, char **argv)
 {
-	double const dt = 0.1;
-	struct mass_storage storage = {100, 10, 2, dt};
+	struct mass_storage storage = {10, 100, 10, .1};
+	double tf = 10;
+	char solver = 'e';
+	switch (argc)
+	{
+		case 7:
+			solver = argv[6][0];
+		case 6:
+			tf = atof(argv[5]);
+		case 5:
+			storage.dt = atof(argv[4]);
+		case 4:
+			storage.damping = atof(argv[3]);
+		case 3:
+			storage.stiffness = atof(argv[2]);
+		case 2:
+			storage.mass = atof(argv[1]);
+		default:
+			printf(
+				"mass:      %f\n"
+				"stiffness: %f\n"
+				"damping:   %f\n"
+				"dt:        %f\n"
+				"tf:        %f\n", storage.mass, storage.stiffness, storage.damping, storage.dt, tf);
+		delay(1000);
+		
+	}
 	struct StrictlyProperBlock block = {2, 1, 2, &storage, outputState, physics, util};
-	double const Xi[2] = {0, 1};
+	double const Xi[2] = {0, 1}; // initial velocity of 0, initial position of 1
 	double * time = NULL;
 	double * Y = NULL;
 	double * U = NULL;
 
-	double const tf = 60;
-	size_t const numSteps = numTimeSteps(dt, tf);
+	size_t const numSteps = numTimeSteps(storage.dt, tf);
 	
 	time = malloc(numSteps * sizeof(double));
 	check_mem(time);
@@ -114,8 +135,18 @@ int main()
 	U = calloc(numSteps, sizeof(double));
 	check_mem(U)
 
-	initializeTime(numSteps, time, dt, 0);
-	euler(&block, Y, dt, numSteps, time, Xi, U);
+	initializeTime(numSteps, time, storage.dt, 0);
+	switch (solver)
+	{
+		case 'e':
+			euler(&block, Y, storage.dt, numSteps, time, Xi, U);
+			break;
+		case 'r':
+			rk4(&block, Y, storage.dt, numSteps, time, Xi, U, U);
+			break;
+		default:
+			printf("No %c solver\n",solver);
+	}
 
 error:
 	free(time);
